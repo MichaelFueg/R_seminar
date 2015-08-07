ebc3e34df61c89f994dff8c9c67d53f06002fdd6
MichaelFueg <michael.fueg@googlemail.com>
2015-08-07 17:16:16 +0200

    Create random Sampler
    Function and help-side are configured

--- MetropolisHastings/R/Random.walk.Metropolis.R
+++ MetropolisHastings/R/Random.walk.Metropolis.R
@@ -1,2 +1,18 @@
 Random.walk.Metropolis <-
-function(){}
+function(n=4, sigma=1, x0=25, N=2000){
+	x <- numeric(N)
+	x[1] <- x0
+	u <- runif(N)
+	k <- 0
+	for (i in 2:N) {
+		y <- rnorm(1, x[i-1], sigma)
+		if (u[i] <= (dt(y, n) / dt(x[i-1], n))) {
+			x[i] <- y
+		}
+		else{
+			x[i] <- x[i-1]
+			k <- k + i
+		}
+	}
+	return(list(simulation=x, events=k))
+}
